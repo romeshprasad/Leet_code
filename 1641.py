c51a6bf4222380bb2c19b7814f9f7d3e28f933d0
romeshprasad <56453677+romeshprasad@users.noreply.github.com>
2023-09-08 21:23:12 -0600

    CodeSolved

--- 1641.py
+++ 1641.py
@@ -1,4 +1,11 @@
 def vowels_count(n):
+    """
+    input = integer
+    This function gets input as an integer n 
+    and returns the length of all the possible combination of vowels of size n sorted lexicographically  
+    output = length of string
+    """
+    
     dp = [[i for i in range(5,0,-1)] for _ in range(n)]
 
 
@@ -6,7 +13,6 @@ def vowels_count(n):
     for j in range(1,n):
         for k in range(3,-1,-1):
             dp[j][k] = dp[j-1][k] + dp[j][k+1]
-            print(dp)
          
     return dp[j][0]
 
